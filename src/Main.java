768d4b2a2a2e623d8cc8ee2a97992c87c5a84cf9
stalkerpnv <stalker_pnv@mail.ru>
2023-04-24 13:07:34 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        int [] arrTwo = new int[5];
+        for (int i = 0; i < arrTwo.length; i++) {
+            arrTwo[i] = i;
+        }
+        int [][] arrD = new int[][]{{1,2},{3,4}};
+        arrD = new int[][]{arr,arrTwo};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.deepToString(arrD));
+    }
+}
